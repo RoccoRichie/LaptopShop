c30b7c6ec12bf394bda7a2f4d69814c140324fb6
Richie Loughran <A00236973@student.ait.ie>
2016-05-31 21:34:48 +0100

    Test Updated to include Arquillian Comments from Whogrego

--- target/generated-sources/annotations/com/laptopshop/laptopshop/model/Laptop_.java
+++ target/generated-sources/annotations/com/laptopshop/laptopshop/model/Laptop_.java
@@ -1,23 +1,21 @@
 package com.laptopshop.laptopshop.model;
 
-import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(Laptop.class)
 public abstract class Laptop_ {
 
-	public static volatile SingularAttribute<Laptop, String> lapMem;
-	public static volatile SingularAttribute<Laptop, Integer> id;
+	public static volatile SingularAttribute<Laptop, String> lapName;
 	public static volatile SingularAttribute<Laptop, Integer> numUsb;
 	public static volatile SingularAttribute<Laptop, Double> price;
-	public static volatile SingularAttribute<Laptop, String> harddrive;
-	public static volatile SingularAttribute<Laptop, String> brand;
-	public static volatile SingularAttribute<Laptop, String> graphics;
 	public static volatile SingularAttribute<Laptop, String> display;
+	public static volatile SingularAttribute<Laptop, Integer> id;
+	public static volatile SingularAttribute<Laptop, String> graphics;
+	public static volatile SingularAttribute<Laptop, String> lapMem;
+	public static volatile SingularAttribute<Laptop, String> brand;
 	public static volatile SingularAttribute<Laptop, String> processor;
-	public static volatile SingularAttribute<Laptop, String> lapName;
+	public static volatile SingularAttribute<Laptop, String> harddrive;
 
 }
 
